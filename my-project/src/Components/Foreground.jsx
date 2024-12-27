e09045ef8bca5b948d78cc81d3dd393082b26244
shaurya2412 <shauryaa2412@gmail.com>
2024-12-27 23:33:14 +0530

    Made all the references and props

--- my-project/src/Components/Foreground.jsx
+++ my-project/src/Components/Foreground.jsx
@@ -1,5 +1,19 @@
+import { FaTag } from "react-icons/fa"
 import Card from "./Card"
 const Foreground = () => {
+
+  const data = [
+    {
+      desc: "lorem ipsum deums ue sied scbk aue sicd ujsw dua ha",
+      filesize: ".9mb",
+      close: true,
+      tag: {isOpen: true, tagTitle: "Download Now", tagColor: "green"},
+    }
+  ]
+
+
+
+
    return (
     <div className="fixed top-0 left-0 z-[3] w-full h-full">
         <Card/>
