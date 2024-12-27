a62019af5bebb52c610f0ee6976ae2b2fe00bf24
shaurya2412 <shauryaa2412@gmail.com>
2024-12-27 22:41:01 +0530

    changes in the card component

--- my-project/src/Components/Card.jsx
+++ my-project/src/Components/Card.jsx
@@ -1,8 +1,11 @@
+import { FaBeer } from 'react-icons/fa';
 
 const Card = () => {
   return (
-    <div className='w-60 h-60 rounded-3xl bg-slate-200 mt-20 ml-20'>
-          <h1 className="ml-20 mt-20 flex justify-between place-content-center">d</h1>
+    <div className='w-60 h-80 rounded-[60px] bg-zinc-900 mt-20 ml-20 ' >
+         <div className=" ml-12 pt-20 text-white flex justify-between place-content-center">
+          <FaBeer size={30} /></div> 
+          <div className='text-white'>wuefvyb</div>
     </div>
                 
 
